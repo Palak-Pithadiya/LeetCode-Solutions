3a14a930e689b1b74e7fcaffaf167e2aa86b7988
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-03-23 17:30:39 +0530

    Add deleteKthElement function in linked list
    Implement a function to delete the k-th element from a linked list.

---
+++ LinkedList/Delete/DeleteKthElement.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int data;
+  struct Node* next;
+}Node;
+
+Node* createNode(int data1) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  if(newNode == NULL) {
+    return NULL;
+  }
+  newNode->data = data1;
+  newNode->next = NULL;
+  return newNode;
+}
+
+Node* convertArr2LL(int arr[], int size) {
+  if(size == 0) return NULL;
+
+  Node* head = createNode(arr[0]);
+  Node* mover = head;
+
+  for(int i = 1; i < size; i++) {
+    Node* temp = createNode(arr[i]);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
+}
+
+void print(Node* head) {
+  Node* temp = head;
+  while(temp) {
+    printf("%d ", temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+}
+
+Node* deletePosEle(Node* head, int k) {
+  if(head == NULL) return NULL;
+  if (k == 1) {
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;  
+  }
+
+  Node* temp = head;
+  Node* prev = NULL;
+  int cnt = 0;
+
+  while(temp != NULL) {
+    cnt++;
+    if(cnt == k){
+        prev->next = temp->next;
+        free(temp);
+        break;
+    }
+    prev = temp;
+    temp = temp->next;
+  }
+
+  return head;
+}
+
+int main() {
+  int arr[] = {2, 3, 4, 5, 6};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  Node* head = convertArr2LL(arr, size);
+  int k = 2;
+  head = deletePosEle(head, k);
+  print(head);
+  
+  return 0;
+}
