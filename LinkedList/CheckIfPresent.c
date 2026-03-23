48b8a02eba7514f32846e71a597b34a8e67e4bc5
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-03-23 15:47:54 +0530

    Add linked list creation and presence check function
    Implement linked list creation and check for presence of a value.

---
+++ LinkedList/CheckIfPresent.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int data;
+  struct Node* next;
+}Node;
+
+Node* createNode(int data1) {
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  
+  if (newNode == NULL) return NULL;
+  newNode->data = data1;
+  newNode->next = NULL;
+  return newNode;
+}
+
+Node* convertAll2LL(int arr[], int size) {
+  if (size == 0) return NULL;
+
+  Node* head = createNode(arr[0]);
+  Node* mover = head;
+
+  for(int i = 1; i < size; i++) {
+    Node* temp = createNode(arr[i]);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
+}
+
+int checkIfPresent(Node* head, int val){
+  Node* temp = head;
+  while(temp) {
+    if(temp->data == val) {
+      return 1;
+    }
+    temp = temp->next;
+  }
+  return 0;
+}
+
+int main() {
+  int arr[] = {2, 3, 4, 5, 6};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  Node* head = convertAll2LL(arr, size);
+  printf("%d ", checkIfPresent(head, 2));
+  
+  return 0;
+}
