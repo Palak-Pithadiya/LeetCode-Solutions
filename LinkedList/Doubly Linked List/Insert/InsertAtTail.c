bddf62660c638cc97aa99f1dda05551858e854f3
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-03-24 18:01:02 +0530

    Implement insertTail function for doubly linked list

---
+++ LinkedList/Doubly Linked List/Insert/InsertAtTail.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure
+typedef struct Node{
+  int data;
+  struct Node* next;
+  struct Node* prev;
+}Node;
+
+Node* convertArr2DLL(int arr[], int size) {
+  if(size == 0) return NULL;
+  Node* head = (Node*)malloc(sizeof(Node));
+  head->data = arr[0];
+  head->prev = NULL;
+  head->next = NULL;
+  
+  Node* mover = head;
+
+  for(int i = 1; i < size; i++) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = arr[i];
+    
+    mover->next = temp;
+    temp->prev = mover;
+    temp->next = NULL;
+    mover = temp;
+  }
+  return head;
+}
+
+Node* printForward(Node* head) {
+  Node* temp = head;
+  while(temp) {
+    printf("%d ",temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+}
+
+Node* printBackward(Node* head) {
+  if(head == NULL) return NULL;
+
+  // Go to the tail
+  Node* temp = head;
+  while(temp->next != NULL) {
+    temp = temp->next;
+  }
+
+  // Traverse backward using 'prev'
+  while(temp) {
+    printf("%d ", temp->data);
+    temp = temp->prev;
+  }
+  printf("\n");
+}
+
+Node* insertTail(Node* head, int val) {
+  if(head == NULL) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(newNode == NULL) return NULL;
+    newNode->data = val;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    
+    return newNode;
+  }
+  
+  Node* temp = head;
+  while(temp->next != NULL) {
+    temp = temp->next;
+  }
+  Node* newNode = (Node*)malloc(sizeof(Node));
+  if(newNode == NULL) return NULL;
+  
+  newNode->data = val;
+  newNode->next = NULL;
+  newNode->prev = temp;
+
+  temp->next = newNode;
+  return head;
+}
+
+int main() {
+  int arr[] = {2, 3, 4, 5};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  Node* head = convertArr2DLL(arr, size);
+  head = insertTail(head, 1);
+  printForward(head);
+  return 0;
+}
