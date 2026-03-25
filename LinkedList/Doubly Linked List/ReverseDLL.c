1bc52180605a90e9a03b246328c8d7fcdaa0aee9
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-03-25 12:05:13 +0530

    Implement reverse function for doubly linked list

---
+++ LinkedList/Doubly Linked List/ReverseDLL.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// structure
+typedef struct Node{
+  int data;
+  struct Node* next;
+  struct Node* prev;
+}Node;
+
+Node* convertArr2DLL(int arr[], int size) {
+  if(size == 0) return NULL;
+  Node* head = (Node*)malloc(sizeof(Node));
+  head->data = arr[0];
+  head->prev = NULL;
+  head->next = NULL;
+  
+  Node* mover = head;
+
+  for(int i = 1; i < size; i++) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = arr[i];
+    
+    mover->next = temp;
+    temp->prev = mover;
+    temp->next = NULL;
+    mover = temp;
+  }
+  return head;
+}
+
+Node* printForward(Node* head) {
+  Node* temp = head;
+  while(temp) {
+    printf("%d ",temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+}
+
+Node* printBackward(Node* head) {
+  if(head == NULL) return NULL;
+
+  // Go to the tail
+  Node* temp = head;
+  while(temp->next != NULL) {
+    temp = temp->next;
+  }
+
+  // Traverse backward using 'prev'
+  while(temp) {
+    printf("%d ", temp->data);
+    temp = temp->prev;
+  }
+  printf("\n");
+}
+
+Node* reverseDLL(Node* head) {
+  if (head == NULL || head->next == NULL) {
+      return head;
+  }
+  Node* temp = head;
+  Node* last = NULL;
+
+  while(temp) {
+    last = temp->prev;
+    temp->next = last;
+    temp->prev = temp->next;
+    temp = temp->prev;
+  }
+  return last->prev;
+}
+
+int main() {
+  int arr[] = {2, 3, 4, 5};
+  int size = sizeof(arr) / sizeof(arr[0]);
+  Node* head = convertArr2DLL(arr, size);
+  head = reverseDLL(head);
+  printForward(head);
+  return 0;
+}
