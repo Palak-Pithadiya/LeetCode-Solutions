bd2bfa86770053267d6770f1400da4b0e0230272
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-03-31 18:55:20 +0530

    Refactor odd check using bitwise operation

--- Bit Manipulation/Check if a Number is Odd or Not.py
+++ Bit Manipulation/Check if a Number is Odd or Not.py
@@ -1,7 +1,16 @@
 class Solution:
     def isOdd(self, n: int) -> bool:
         # Your code goes here
-        if(n % 2 == 1):
+        if(n & 1 == 1):
             return True
         else:
             return False
+
+"""
+let n = 13
+
+  n = 1 0 1 1 
+& 1 = 0 0 0 1
+--------------
+      0 0 0 1
+"""
