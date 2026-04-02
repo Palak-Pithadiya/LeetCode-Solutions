5323ae67e2f8ec75d9ffc0e2154a6ab64dd19009
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-04-02 14:47:35 +0530

    Create Prefix to Infix Conversion.py
    You are given a valid arithmetic expression in prefix notation. Your task is to convert it into a fully parenthesized infix expression.
    
    Prefix notation (also known as Polish notation) places the operator before its operands. In contrast, infix notation places the operator between operands.
    
    Your goal is to convert the prefix expression into a valid fully parenthesized infix expression.

---
+++ Stack & Queues/Prefix to Infix Conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def prefixToInfix(self, s: str) -> str:
+        # Your code goes here
+        st = []
+        i = len(s) - 1
+
+        while i >= 0:
+            char = s[i]
+
+            if (char >= 'A' and char <= 'Z') or (char >= 'a' and char <= 'z') or (char >= '0' and char <= '9'):
+                st.append(char)
+
+            else:
+                t1 = st.pop()
+                t2 = st.pop()
+
+                con = '(' + t1 + char + t2 + ')'
+                st.append(con)
+
+            i -= 1
+
+        return st[0]
