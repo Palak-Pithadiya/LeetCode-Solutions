2f31be0948c4f6dd751ed1b703d9a349f6d246a0
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-04-02 14:59:41 +0530

    Create Postfix to Infix Conversion.py
    You are given a valid postfix expression as a string, where:
    Operands are single lowercase English letters ('a' to 'z')
    Operators are binary: '+', '-', '*', '/'
    
    The expression contains no spaces and is guaranteed to be syntactically valid.
    Write a function to convert the given postfix expression into a valid infix expression.
    
    Use parentheses to clearly represent the evaluation order of the expression.

---
+++ Stack & Queues/Postfix to Infix Conversion.py
@@ -0,0 +1,21 @@
+class Solution:
+    def post_to_infix(self, s: str) -> str:
+        # Your code goes here
+        st = []
+        i = 0
+
+        while i < len(s):
+            char = s[i]
+
+            if (char >= 'A' and char <= 'Z') or (char >= 'a' and char <= 'z') or (char >= '0' and char <= '9'):
+                st.append(char)
+
+            else:
+                t1 = st.pop()
+                t2 = st.pop()
+
+                con = '(' + t2 + char + t1 + ')'
+                st.append(con)
+            i += 1
+        
+        return st[0] if st else ''
