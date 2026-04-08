f8cd8908297b9ca958a5a9bd07914f8c80deb901
Palak-Pithadiya <palak.pithadiya20@gmail.com>
2026-04-08 18:12:06 +0530

    Create 101. Symmetric Tree.py
    Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).

---
+++ Binary Trees/101. Symmetric Tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        queue = deque([(root.left, root.right)])
+        
+        while queue:
+            t1, t2 = queue.popleft()
+
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2 or t1.val != t2.val:
+                return False
+
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+
+        return True
